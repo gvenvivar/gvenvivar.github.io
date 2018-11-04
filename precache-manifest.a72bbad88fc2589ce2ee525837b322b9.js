b0ad8e7c70f84d4defb8c758965ec27b9785b7fe
gven <gven.inc@gmail.com>
2018-11-04 15:19:26 +0200

    update react on build

---
+++ precache-manifest.a72bbad88fc2589ce2ee525837b322b9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d5c2bec0b7f20b3682c872a7ec3d492",
+    "url": "/static/media/loading2.7d5c2bec.gif"
+  },
+  {
+    "revision": "830ac1e32ba1277d52827756175377ee",
+    "url": "/static/media/frostmourne_by_chillalord-d5ka9vy_dark.830ac1e3.jpg"
+  },
+  {
+    "revision": "ae7ab87e49e4be9a320e652134749ee7",
+    "url": "/static/media/close.ae7ab87e.png"
+  },
+  {
+    "revision": "ed73789884f7826d794c054d0918bea2",
+    "url": "/static/media/cerrar2.ed737898.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f1c02d583317094e656a",
+    "url": "/static/js/main.f1c02d58.chunk.js"
+  },
+  {
+    "revision": "ec391210be25412ab94e",
+    "url": "/static/js/1.ec391210.chunk.js"
+  },
+  {
+    "revision": "f1c02d583317094e656a",
+    "url": "/static/css/main.4e0cfaba.chunk.css"
+  },
+  {
+    "revision": "d76a3fd61634f2dded2068285b532eb0",
+    "url": "/index.html"
+  }
+];
